455d86c9ed3ffd8f73c6da02b09841fd6e23f934
KanishkNavale <navalekanishk@gmail.com>
2022-11-11 23:05:42 +0100

    Added: bi-pair image augmentation and tracking

--- methods.py
+++ methods.py
@@ -5,6 +5,11 @@ import torch
 import torchvision.transforms as T
 
 
+# Image Augmentation
+image_augmentator = T.Compose([T.RandomAffine(degrees=170, translate=(0, 0.2)),
+                               T.RandomPerspective(distortion_scale=0.2, p=0.5)])
+
+
 def stack_image_with_spatialgrid(images: torch.Tensor) -> torch.Tensor:
     us = torch.arange(0, images.shape[-2], 1, dtype=torch.float32, device=images.device)
     vs = torch.arange(0, images.shape[-1], 1, dtype=torch.float32, device=images.device)
@@ -18,23 +23,16 @@ def stack_image_with_spatialgrid(images: torch.Tensor) -> torch.Tensor:
 
 def destack_image_with_spatialgrid(augmented_image: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
     image = augmented_image[:, :3, :, :]
-    spatial_grid = augmented_image[:, 3:, :]
+    spatial_grid = torch.round(augmented_image[:, 3:, :])
 
     return image, spatial_grid
-
-
-def get_random_augmentation(image: torch.Tensor) -> torch.Tensor:
-    if 0 == np.random.randint(low=0, high=1):
-        return T.RandomAffine(degrees=60, translate=(0, 0.5))(image)
-    else:
-        return T.RandomPerspective(distortion_scale=0.5, p=1.0)(image)
 
 
 def augment_images_and_map_correspondence(images: torch.Tensor,
                                           n_correspondence: int) -> torch.Tensor:
 
-    augmented_image_a = stack_image_with_spatialgrid(images)
-    augmented_image_b = get_random_augmentation(stack_image_with_spatialgrid(images))
+    augmented_image_a = image_augmentator(stack_image_with_spatialgrid(images))
+    augmented_image_b = image_augmentator(stack_image_with_spatialgrid(images))
 
     augmented_images_a, grids_a = destack_image_with_spatialgrid(augmented_image_a)
     augmented_images_b, grids_b = destack_image_with_spatialgrid(augmented_image_b)
@@ -45,25 +43,25 @@ def augment_images_and_map_correspondence(images: torch.Tensor,
     # Compute correspondence from the spatial grid
     for grid_a, grid_b in zip(grids_a, grids_b):
 
-        valid_pixels_a = torch.where(grid_a.mean(dim=0) != 0.0)
+        valid_pixels_a = torch.where(grid_a.sum(dim=0) != 0.0)
 
         us = valid_pixels_a[0]
         vs = valid_pixels_a[1]
 
         # Reducing computation costs
-        trimming_indices = torch.linspace(0, us.shape[0] - 1, steps=5 * n_correspondence)
-        trimming_indices = trimming_indices.type(torch.int64)
-        us = us[trimming_indices].type(torch.float32)
-        vs = vs[trimming_indices].type(torch.float32)
+        trimming_indices = torch.linspace(0, us.shape[0] - 1, steps=10 * n_correspondence)
+        us = us[trimming_indices.long()].type(torch.float32)
+        vs = vs[trimming_indices.long()].type(torch.float32)
 
         valid_pixels_a = torch.vstack([us, vs]).permute(1, 0)
-        tiled_valid_pixels_a = valid_pixels_a.view(valid_pixels_a.shape[0], valid_pixels_a.shape[1], 1, 1)
+        valid_grids_a = grid_a[:, us.long(), vs.long()].permute(1, 0)
+        tiled_valid_grids_a = valid_grids_a.view(valid_grids_a.shape[0], valid_grids_a.shape[1], 1, 1)
 
-        spatial_grid_distances = torch.linalg.norm(torch.round(grid_b) - tiled_valid_pixels_a, dim=1)
+        spatial_grid_distances = torch.linalg.norm(grid_b - tiled_valid_grids_a, dim=1)
 
-        match_indices_a, ubs, vbs = torch.where(spatial_grid_distances == 0.0)
+        match_indices_a, ubs, vbs = torch.where(spatial_grid_distances <= 0.1)
 
-        mutual_match_a = valid_pixels_a[match_indices_a]
+        mutual_match_a = valid_pixels_a[match_indices_a.long()]
         mutual_match_b = torch.vstack([ubs, vbs]).permute(1, 0)
 
         trimming_indices = torch.linspace(0, mutual_match_a.shape[0] - 1, steps=n_correspondence)
